594f13d057a32bed2dc40dbae9822a56200174b2
Lucas Raimundo <lucasraimundo2010@gmail.com>
2025-02-12 15:37:31 -0300

    creating thoughts

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,28 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit{
 
-  listaPensamentos = [
-    {
-      conteudo: 'I love code',
-      autoria: 'Luqueta',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'I love angular',
-      autoria: 'Luqueta',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      autoria: 'Luqueta',
-      modelo: 'modelo2'
-    }
-  ]
+  listaPensamentos: Pensamento[] = []
 
-  constructor(){
+  constructor(private service: PensamentoService){
   }
 
   ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    });
   }
 
 }
