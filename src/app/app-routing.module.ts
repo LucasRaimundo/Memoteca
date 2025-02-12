c547d02fc0e4c9f8111e3ac749f8fb3e1b0be290
Lucas Raimundo <lucasraimundo2010@gmail.com>
2025-02-12 17:55:27 -0300

    update post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   {
@@ -23,6 +24,10 @@ const routes: Routes = [
   {
     path: 'pensamentos/excluirPensamentos/:id',
     component: ExcluirPensamentoComponent
+  },
+  {
+    path: 'pensamentos/editarPensamentos/:id',
+    component: EditarPensamentoComponent
   },
 
 ];
